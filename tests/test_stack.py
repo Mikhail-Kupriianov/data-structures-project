e8f497ab80b9266a71349acecf7ed58092e789da
Mkupr <mkupr@outlook.com>
2023-03-21 22:18:24 +0300

    replace pytest with unittest for test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,35 +1,56 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
-import pytest
+import unittest
+
+import src.stack
+# import pytest
 from src.stack import Node, Stack
 
 
-@pytest.fixture
-def test_data():
-    return [10, 'data', 'Test']
+# @pytest.fixture
+# def test_data():
+#     return [10, 'data', 'Test']
+
+class TestStack(unittest.TestCase):
+
+    def test_node_init(self):
+        n1 = Node(10)
+        n2 = Node('data', n1)
+        self.assertEqual(n1.data, 10)
+        self.assertEqual(n2.data, 'data')
+        self.assertEqual(n2.next_node, n1)
+
+    # def test_node_init(test_data):
+    #     n1 = Node(test_data[0])
+    #     n2 = Node(test_data[1])
+    #     assert n1.data == 10
+    #     assert n2.data == 'data'
+
+    # def test_pointer_node(test_data):
+    #     n1 = Node(test_data[0])
+    #     n2 = Node(test_data[1], n1)
+    #     n3 = Node(test_data[2], n2)
+
+    # assert n1.next_node is None
+    # assert n2.next_node == n1
+    # assert n3.next_node == n2
+
+    def test_push(self):
+        stack = Stack()
+        stack.push(10)
+        stack.push('data')
+        stack.push('Test')
+        self.assertEqual(stack.top.data, 'Test')
+        self.assertEqual(stack.top.next_node.data, 'data')
+        self.assertEqual(stack.top.next_node.next_node.data, 10)
+        self.assertEqual(stack.top.next_node.next_node.next_node, None)
+
+    # for i in test_data:
+    #
+    # assert stack.top.data == 'Test'
+    # assert stack.top.next_node.data == 'data'
+    # assert stack.top.next_node.next_node.data == 10
+    # assert stack.top.next_node.next_node.next_node is None
 
 
-def test_node_init(test_data):
-    n1 = Node(test_data[0])
-    n2 = Node(test_data[1])
-    assert n1.data == 10
-    assert n2.data == 'data'
-
-
-def test_pointer_node(test_data):
-    n1 = Node(test_data[0])
-    n2 = Node(test_data[1], n1)
-    n3 = Node(test_data[2], n2)
-
-    assert n1.next_node is None
-    assert n2.next_node == n1
-    assert n3.next_node == n2
-
-
-def test_push(test_data):
-    stack = Stack()
-    for i in test_data:
-        stack.push(i)
-    assert stack.top.data == 'Test'
-    assert stack.top.next_node.data == 'data'
-    assert stack.top.next_node.next_node.data == 10
-    assert stack.top.next_node.next_node.next_node is None
+if __name__ == '__main__':
+    unittest.main()
